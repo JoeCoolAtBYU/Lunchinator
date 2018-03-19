aac74f5f89046d14a6ab82ecb12c96068c640ed5
Joshua <joecoolatbyu@hotmail.com>
2018-03-18 23:20:20 -0600

    Created Test for Controller and created method in ballot to return JSON only for the ID.

--- src/main/java/barnett/joshua/lunchinator/Application.java
+++ src/main/java/barnett/joshua/lunchinator/Application.java
@@ -1,33 +1,37 @@
 package barnett.joshua.lunchinator;
 
 import org.springframework.boot.Banner.Mode;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableSwagger2
 public class Application extends SpringBootServletInitializer {
 
-   /**
-    * Allows the app to run as a web app in an executable fat jar.
-    *
-    * @param args
-    */
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplicationBuilder(Application.class).bannerMode(Mode.OFF).build();
-        app.run(args);
+        SpringApplication.run(Application.class, args);
     }
 
-    /**
-     * Allows the app to run as a web application in a war file.
-     */
-    @Override
-    protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
-       application.sources(Application.class);
-       application.bannerMode(Mode.OFF);
-       return super.configure(application);
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
     }
 }
